9205fd51f2221ec48726f3a2d15cd3016c5e1b61
gregorkschroeder <gregorkschroeder@gmail.com>
2019-06-10 10:18:19 -0700

    update initial database build

--- resources/db/build_db.bat
+++ resources/db/build_db.bat
@@ -7,10 +7,21 @@ rem db_path and log_path values must be enclosed in double quotes
 set db_path=
 set log_path=
 
-
-cd ../sql/db
-
+rem assumes running from project directory
+cd sql/db
 
 echo Creating %db_name% on %db_server% at %db_path%
 echo Log file at %log_path%
 sqlcmd -E -C -b -S %db_server% -i create_db.sql -v db_path=%db_path% log_path=%log_path%
+
+echo Adding metadata schema
+sqlcmd -E -C -b -S %db_server% -d %db_name% -i db_meta.sql
+
+echo Adding ABM database synonyms
+sqlcmd -E -C -b -S %db_server% -d %db_name% -i synonyms.sql
+
+rem move back to project directory
+cd ..
+cd ..
+
+echo Finished creating %db_name% on %db_server%
